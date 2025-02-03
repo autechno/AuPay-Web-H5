00e48b0b45224c598d588c0d6dd92278378ca6b9
yoney-zhang <yoney.zhang@autech.one>
2025-02-03 14:29:37 +0800

    兑换面页重构

---
+++ themes/pages/assets-account-old/index.vue
@@ -0,0 +1,382 @@
+<template>
+  <div class="page">
+    <div class="avatar-header">
+      <div class="avatar" style="float: left"><img v-if="userInfo.headPortrait" style="width: 100%;"  :src="userInfo.headPortrait" /></div>
+      <span class="name">{{userInfo.name}}</span>
+      <div class="avatar" style="float: right">
+        <img :src="scan" class="scan" />
+      </div>
+    </div>
+    <div class="main">
+      <div @click="setConfigShow">
+        <span style="color: #6E6E6E; font-size: 14px;">你的总资产<img :src="eye"  style="vertical-align: middle; width: 16px;"></span>
+        <p style="height: 49px; font-weight: bold; font-size: 36px; line-height: 49px;">
+          {{!isShowCurrency ? '******' : userInfo.currencySign + formatCurrency(totalAssets) }}
+        </p>
+      </div>
+      <el-row :gutter="20" class="icon-container">
+        <el-col :span="6">
+          <p class="icon-text">
+            <i class="i1"></i>
+            <span>充值</span>
+          </p>
+        </el-col>
+        <el-col :span="6">
+          <p class="icon-text">
+            <i class="i2"></i>
+            <span>提现</span>
+          </p>
+        </el-col>
+        <el-col :span="6">
+          <p class="icon-text">
+            <i class="i3"></i>
+            <span>转帐</span>
+          </p>
+        </el-col>
+        <el-col :span="6">
+          <p class="icon-text">
+            <i class="i4"></i>
+            <span>转帐</span>
+          </p>
+        </el-col>
+      </el-row>
+      <div class="banner"> <img :src="banner" /> </div>
+      <div class="table-title"><span style="font-size: 18px;">我的代币</span><span style="font-size: 12px; color: #FDC92E">所有代币</span></div>
+      <div class="table-list">
+        <div class="item" v-for="(item, index) in currencyMergedData" :key="index">
+          <div class="left-column">
+            <div class="currency-wrap"></div>
+          </div>
+          <div class="right-column">
+            <p class="row"><span class="title">{{ item.currencyJson.name }}</span> <span class="title">{{formatCurrency(totalAssets)}}</span></p>
+            <p class="row"><span class="text">{{!isShowCurrency ? '******' : userInfo.currencySign + formatCurrency(item.balance) }}</span> <span class="text" style="color: #0F9A50">+2000U</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <el-row :gutter="20" class="home-container" style="margin-left:0; margin-right: 0">
+      <el-col :span="6">
+        <p class="icon-text">
+          <i class="i1"></i>
+          <span>首页</span>
+        </p>
+      </el-col>
+      <el-col :span="6">
+        <p class="icon-text">
+          <i class="cur2"></i>
+          <span>资产</span>
+        </p>
+      </el-col>
+      <el-col :span="6">
+        <p class="icon-text">
+          <i class="i3"></i>
+          <span>行情</span>
+        </p>
+      </el-col>
+      <el-col :span="6">
+        <p class="icon-text">
+          <i class="i4"></i>
+          <span>用户</span>
+        </p>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+import banner from '@@/public/images/banner.png';
+import { getHeader } from "@/utils/storageUtils";
+import eye from "@@/public/images/eye3x.svg";
+import scan from "@@/public/images/Scan.svg";
+import {
+  formatCurrency,
+  getDataInfo,
+} from "@/utils/formatUtils";
+import {ElMessage} from "element-plus";
+const headers = getHeader();
+const { assetsApi, systemApi } = useServer();
+const currencyItemData = ref([]);
+const currencyMergedData = ref([]);
+const totalAssets = ref(0);
+const isShowCurrency = ref(false);
+const userInfo = ref({
+  headPortrait: '',
+  name: '',
+  currencyCode: 'USD',
+  currencySign: '$'
+})
+
+//
+const fetchData = async () => {
+  try {
+    const [rateResponse, assetsResponse] = await Promise.all([
+      assetsApi.getRateU2Currency({ currency: userInfo.value.currencyCode }, headers),
+      assetsApi.accountAssets({}, headers)
+    ]);
+    // 处理汇率响应
+    let exchangeRate = 1;
+    if (rateResponse.code == 200) {
+      exchangeRate = rateResponse.data
+    }
+    // 处理资产响应
+    if (assetsResponse.code == 200) {
+      const mergedData = {};
+      // 初始化总和变量
+      let totalBalanceUsdtSum = 0;
+      const dataList = assetsResponse.data; // 假设数据在 res.data 中
+      if (dataList && dataList.length > 0) {
+        dataList.forEach(item => {
+          item['totalBalanceUsdt'] = item['totalBalanceUsdt'] * exchangeRate;
+          const { currencyId, currencyChain,  balance, freezeBalance, totalBalance, totalBalanceUsdt } = item;
+          let currencyKeyValue =  getDataInfo(currencyId, 'currencyChains');
+          let coinKeyValue =  getDataInfo(currencyChain, 'chains');
+          let mergedStore  = { ...item, currencyJson: currencyKeyValue, coinJson: coinKeyValue };
+          currencyItemData.value.push(mergedStore);
+          if (!mergedData[currencyId]) {
+            mergedData[currencyId] = mergedStore;
+          }else{
+            mergedData[currencyId].balance += balance;
+            mergedData[currencyId].freezeBalance += freezeBalance;
+            mergedData[currencyId].totalBalance += totalBalance;
+            mergedData[currencyId].totalBalanceUsdt += totalBalanceUsdt;
+          }
+          totalBalanceUsdtSum += totalBalanceUsdt;
+        });
+        totalAssets.value = totalBalanceUsdtSum;
+        currencyMergedData.value = Object.values(mergedData);
+      }
+    } else {
+      ElMessage.error(assetsResponse.message || '查询失败');
+    }
+  } catch (error) {
+    console.log(error)
+    console.log(error)
+    console.log(error)
+    ElMessage.error('请求失败，请重试');
+  }
+};
+// 显示隐藏
+const setConfigShow = async () => {
+   isShowCurrency.value = !isShowCurrency.value;
+}
+// 初始化数据
+onMounted(() => {
+  const userStore = UseUserStore();
+  userInfo.value.headPortrait = userStore.userInfo.headPortrait;
+  userInfo.value.currencyCode = userStore.userInfo.currencyUnit;
+  userInfo.value.name = userStore.userInfo.nickname;
+  userInfo.value.currencySign = getDataInfo(userInfo.value.currencyCode, 'currency')?.sign;
+  fetchData();
+})
+
+</script>
+<style scoped>
+*{
+  margin:0;
+  padding: 0;
+  font-size: 12px;
+}
+.page {
+  position: relative;
+  color: #0D0D0D;
+  padding-bottom: 100px;
+}
+.avatar-header{
+  margin-top: 30px;
+  width: 100%;
+  height: 46px;
+}
+.avatar-header .name{
+  font-size: 18px;
+  font-weight: bold;
+  line-height: 46px;
+  padding-left: 5px;
+}
+.avatar-header .avatar{
+  Width: 46px;
+  Height: 46px;
+  background: #F4F4F4;
+  overflow: hidden;
+  border-radius: 50%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.avatar-header .scan {
+  object-fit: cover;
+  width: 24px;
+  height: 24px;
+}
+.main{
+  margin-top: 32px;
+}
+.icon-container{
+  margin-top: 22px;
+  width: 106%;
+  text-align: center;
+}
+.icon-container .icon-text{
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 70px;
+  height: 64px;
+  background: #EAF3FA;
+  overflow: hidden;
+  border-radius: 10px;
+  font-weight: bold;
+  font-size: 12px!important;
+}
+.icon-container  i{
+  display: block;
+  width: 28px;
+  height: 28px;
+  margin: 7px auto 4px auto;
+}
+.icon-container  i.i1{
+  background: url(@@/public/images/cz2.png)  no-repeat;
+  background-size: 100%;
+}
+.icon-container  i.i2{
+  background: url(@@/public/images/tx@2x.png)  no-repeat;
+  background-size: 100%;
+}
+.icon-container  i.i3{
+  background: url(@@/public/images/zh2x.png)  no-repeat;
+  background-size: 100%;
+}
+.icon-container  i.i4{
+  background: url(@@/public/images/sd2x.png)  no-repeat;
+  background-size: 100%;
+}
+.icon-container i img{
+  width: 28px;
+  height: 28px;
+  margin-bottom: 3px;
+}
+
+.home-container{
+  width: calc(100% - 36px);
+  text-align: center;
+  background: #ffffff;
+  height: 88px;
+  border-radius: 30px;
+  box-shadow: 0 12px 20px rgba(109, 110, 124, 0.2);
+  position: fixed;
+  bottom: 16px;
+  left: 18px;
+}
+.home-container .icon-text{
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10px;
+  width: 70px;
+  height: 64px;
+  overflow: hidden;
+  font-weight: bold;
+  font-size: 12px!important;
+  color: #1C63FF;
+}
+.home-container  i{
+  display: block;
+  width: 28px;
+  height: 28px;
+  margin: 7px auto 4px auto;
+}
+.home-container  i.i1{
+  background: url(@@/public/images/Home@2x.png)  no-repeat;
+  background-size: 100%;
+}
+.home-container  i.i2{
+  background: url(@@/public/images/zichan@2x.png)  no-repeat;
+  background-size: 100%;
+}
+.home-container  i.i3{
+  background: url(@@/public/images/hangqing@2x.png)  no-repeat;
+  background-size: 100%;
+}
+.home-container  i.i4{
+  background: url(@@/public/images/wo@2x.png)  no-repeat;
+  background-size: 100%;
+}
+.home-container  i.cur1{
+  background: url(@@/public/images/Home2@3x.png)  no-repeat;
+  background-size: 100%;
+}
+.home-container  i.cur2{
+  background: url(@@/public/images/zichan2@2x.png)  no-repeat;
+  background-size: 100%;
+}
+.home-container  i.cur3{
+  background: url(@@/public/images/hangqing2@2x.png)  no-repeat;
+  background-size: 100%;
+}
+.home-container  i.cur4{
+  background: url(@@/public/images/yonghu2@2x.png)  no-repeat;
+  background-size: 100%;
+}
+.home-container i img{
+  width: 28px;
+  height: 28px;
+}
+
+.table-title {
+  line-height: 20px;
+  overflow: hidden;
+  display: flex;
+  justify-content: space-between;
+}
+.banner{
+  margin-top: 25px;
+  margin-bottom: 25px;
+}
+.banner img{
+  width: 100%;
+}
+.table-list{
+  position: relative;
+  margin-top: 10px;
+}
+.table-list .item{
+  display: flex;
+  height: 52px;
+  padding: 10px 0;
+  border-bottom: 1px solid #f1f1f1;
+  overflow: hidden;
+}
+.left-column {
+  width: 48px;
+  height: 48px;
+}
+.left-column .currency-wrap{
+  width: 100%;
+  height: 100%;
+  background-color: #eaeaea;
+  border-radius: 48px;
+}
+.right-column {
+  padding-left: 10px;
+  flex: 1;
+  padding-top: 5px;
+  justify-content: flex-start;
+  flex-direction: column;
+}
+.right-column .row {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+.right-column .row .title {
+  font-weight: bold;
+  font-size: 16px;
+}
+
+.right-column .row .text {
+  color: #6E6E6E;
+}
+</style>
--- themes/pages/flash-exchange/index.vue
+++ themes/pages/flash-exchange/index.vue
@@ -1,150 +1,403 @@
 <template>
   <div class="page">
-    <GoBack />
-    <div class="content">
-      <div class="exchange-container" style="height: 126px;">
-        <div class="title">兑出</div>
-        <div class="currency">22,684.02</div>
-        <el-row class="flash-btn">
-          <el-col :span="8"><span>50%</span></el-col>
-          <el-col :span="8"><span>100%</span></el-col>
-        </el-row>
-        <div class="select-wrap">
-          <div class="img"></div>
-          <div class="text-wrap">BTC</div>
-          <el-icon size="12" class="icon"> <ArrowDownBold /> </el-icon>
+    <div class="exchange-container exchange-container-wrap">
+      <p>闪兑</p>
+      <div class="exchange-wrapper">
+        <div class="exchange-container">
+          <div class="select-container">
+            <el-select
+                style="margin-bottom: 20px;"
+                id="currency-select-to"
+                v-model="form.selectedCurrencyToId"
+                placeholder="请选择货币"
+                @change="updateCurrencyChain('to')">
+              <el-option
+                  v-for="currency in currencyMergedData"
+                  :key="currency.currencyId"
+                  :label="currency.currency[0].name"
+                  :value="currency.currencyId"
+              />
+            </el-select>
+            <el-select
+                id="chain-select-to"
+                style="margin-bottom: 20px;"
+                v-model="form.selectedChainTo"
+                placeholder="请选择链">
+              <el-option
+                  v-for="chain in form.selectedCurrencyChainTo"
+                  :key="chain.id"
+                  :label="chain.name"
+                  :value="chain.id"
+              />
+            </el-select>
+            <!-- 输入框部分 -->
+            <div class="input-container">
+              <el-input
+                  v-model="form.inputAmountTo"
+                  placeholder="请输入金额"
+                  type="number"
+                  @input="syncInputAmountTo(false)"
+              />
+              <span style="padding-left: 20px;">{{ form.selectedCurrencyTo }}</span>
+            </div>
+            <!-- 显示余额 -->
+            <div class="balance-display">
+              <span>可兑换余额: {{ form.bigNumCost }}</span>
+            </div>
+          </div>
+        </div>
+        <div class="button-container">
+          <el-button @click="swapCurrencies" :disabled="loading" :loading="loading">交换</el-button>
+        </div>
+        <div class="exchange-container">
+          <div class="select-container">
+            <el-select
+                style="margin-bottom: 20px;"
+                id="currency-select"
+                v-model="form.selectedCurrencyId"
+                placeholder="请选择货币"
+                @change="updateCurrencyChain('form')">
+              <el-option
+                  v-for="currency in currencyMergedData"
+                  :key="currency.currencyId"
+                  :label="currency.currency[0].name"
+                  :value="currency.currencyId"
+              />
+            </el-select>
+            <el-select
+                id="chain-select"
+                style="margin-bottom: 20px;"
+                v-model="form.selectedChain"
+                placeholder="请选择链">
+              <el-option
+                  v-for="chain in form.selectedCurrencyChain"
+                  :key="chain.id"
+                  :label="chain.name"
+                  :value="chain.id"
+              />
+            </el-select>
+            <!-- 输入框部分 -->
+            <div class="input-container">
+              <el-input
+                  v-model="form.inputAmount"
+                  placeholder="请输入金额"
+                  type="number"
+                  @input="syncInputAmount"
+              />
+              <span style="padding-left: 20px;">{{ form.selectedCurrency }}</span>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="button"><img :src="fx" /> </div>
-      <div class="exchange-container" style="height: 90px; margin-top:12px;">
-        <div class="title">兑入</div>
-        <div class="currency">22,684.02</div>
+      <div style="text-align: right;">
+        <div>当前兑换汇率：{{ rateExchange.content }}</div>
+        <div v-if="cost.amount">费用：{{ cost.content }}</div>
       </div>
     </div>
-    <div class="tips">
-      <span>前兑换汇率：</span>
-      <span>1 TOTO ≈ 0.2 OZC</span>
-    </div>
-    <div class="tips">
-      <span>费用：</span>
-      <span>10 USDT</span>
-    </div>
-      <el-button class="custom-button" native-type="submit">确认</el-button>
+    <el-button @click="dialogCheckVisible = true" type="primary" >确认兑换</el-button>
+    <!-- 密码验证对话框 -->
+    <CheckPermissionDialog
+        :form="form"
+        @update:form="updateForm"
+        :permissionId="6"
+        :isDialogVisible="dialogCheckVisible"
+        @close="dialogCheckVisible = false"
+    />
+    <List></List>
   </div>
 </template>
 <script setup lang="ts">
-import fx from '@@/public/images/fx.svg';
-import GoBack from "@/composables/GoBack.vue";
-import {ArrowDownBold} from "@element-plus/icons-vue";
+import { ref, onMounted } from 'vue';
+import { getHeader } from "@/utils/storageUtils";
+import { ElMessage} from "element-plus";
+import List from './detail/list.vue'
+import CheckPermissionDialog from "@/composables/CheckPermissionDialog.vue";
+const headers = getHeader();
+const { assetsApi } = useServer();
+// 处理整合数据列表
+const currencyMergedData = ref([]);
+const  dialogCheckVisible = ref(false);
+const  rateExchange = ref({
+  content: '',
+  rate: 1,
+});
+const cost = ref({
+  content: '',
+  amount: 0,
+});
+
+const loading = ref(false); // Add loading state
+
+
+// 状态合并到一个对象中
+const form = ref({
+  selectedCurrencyId: null,
+  selectedCurrency: null,
+  selectedChain: null,
+  selectedCurrencyChain: [],
+  inputAmount: '',
+  bigNumCost: 0,
+
+  selectedCurrencyToId: null,
+  selectedCurrencyTo: null,
+  selectedChainTo: null,
+  selectedCurrencyChainTo: [],
+  inputAmountTo: '',
+  bigNumCostTo: 0,
+
+  bindPassword: false,
+  googleCode: '',
+  assetsPassword: '',
+  passwordToken: '',
+  googleToken: '',
+  optToken: '',
+});
+
+// 更新父组件的 form 数据
+const updateForm = async (newForm: Object) => {
+  try {
+    form.value = newForm;
+    dialogCheckVisible.value = false;
+    if(form.value.permissionStatus){
+      setHeadersAuth(headers, form);
+      let res = await assetsApi.fastSwapApply({
+        optToken: form.value.optToken,
+        outCurrencyId: form.value.selectedCurrencyToId,
+        outChain: form.value.selectedChainTo,
+        inCurrencyId: form.value.selectedCurrencyId,
+        inChain: form.value.selectedChain,
+        transOutAmount: form.value.inputAmountTo,
+        transInAmount: form.value.inputAmount,
+      }, headers);
+      if(res.code == 200) {
+        ElMessage.success("闪兑成功")
+      }else{
+        ElMessage.error(res.message)
+      }
+    }
+  } catch (error) {
+    ElMessage.error('请求失败，请重试');
+  }
+};
+
+// 获取数据的函数
+const fetchData = async () => {
+  try {
+    let res = await assetsApi.accountAssets({}, headers);
+    if (res.code === 200) {
+      const mergedData = new Map();
+      const dataList = res.data;
+      if (dataList?.length) {
+        dataList.forEach(item => {
+          const { currencyId, currencyChain, balance, totalBalanceUsdt } = item;
+          // 检查是否已存在该货币数据
+          if (!mergedData.has(currencyId)) {
+            mergedData.set(currencyId, {
+              balance,
+              totalBalanceUsdt,
+              currencyId,
+              currencyChain: [{ id: currencyChain, name: getDataInfo(currencyChain, 'chains')?.name }],
+              currency: [{ id: currencyId, name: getDataInfo(currencyId, 'currencyChains')?.name,}],
+            });
+          } else {
+            const existingData = mergedData.get(currencyId);
+            if (!existingData.currencyChain.some(chain => chain.id === currencyChain)) {
+              existingData.currencyChain.push({ id: currencyChain, name: getDataInfo(currencyChain, 'chains')?.name });
+            }
+            existingData.balance += balance;
+            existingData.totalBalanceUsdt += totalBalanceUsdt;
+          }
+        });
+        currencyMergedData.value = Array.from(mergedData.values());
+        // 如果有可用的货币，自动选择第一个
+        if (currencyMergedData.value.length > 0) {
+          form.value.selectedCurrencyId = currencyMergedData.value[0].currencyId;
+          updateCurrencyChain('form');
+          form.value.selectedCurrencyToId = currencyMergedData.value[0].currencyId;
+          updateCurrencyChain('to');
+        }
+      }
+    } else {
+      ElMessage.error(res.message || '查询失败');
+    }
+  } catch (error) {
+    ElMessage.error('请求失败，请重试');
+  }
+};
+
+// 更新链数据的函数
+const updateCurrencyChain = (formType: string) => {
+  // 切换清空金额内容
+  form.value.inputAmountTo = '';
+  form.value.inputAmount = '';
+  cost.value = { amount: 0, content: '' };
+  const selectedCurrencyData = formType === 'form'
+      ? currencyMergedData.value.find(currency => currency.currencyId === form.value.selectedCurrencyId)
+      : currencyMergedData.value.find(currency => currency.currencyId === form.value.selectedCurrencyToId);
+  if (formType === 'form') {
+    form.value.selectedCurrency = selectedCurrencyData.currency[0].name;
+    form.value.selectedCurrencyChain = selectedCurrencyData ? selectedCurrencyData.currencyChain : [];
+    form.value.selectedChain = form.value.selectedCurrencyChain.length > 0 ? form.value.selectedCurrencyChain[0].id : null;
+  } else {
+    form.value.selectedCurrencyTo = selectedCurrencyData.currency[0].name;
+    form.value.selectedCurrencyChainTo = selectedCurrencyData ? selectedCurrencyData.currencyChain : [];
+    form.value.selectedChainTo = form.value.selectedCurrencyChainTo.length > 0 ? form.value.selectedCurrencyChainTo[0].id : null;
+    form.value.bigNumCost = selectedCurrencyData.balance
+  }
+  if(form.value.selectedCurrencyId && form.value.selectedCurrencyToId){
+    fetchRateExchange();
+  }
+};
+
+// 获取数据的函数
+const fetchRateExchange = async () => {
+  try {
+    // 按 ID 匹配并找到名称
+    let res = await assetsApi.getRateCoin2Coin({ sourceCurrencyId: form.value.selectedCurrencyToId, targetCurrencyId: form.value.selectedCurrencyId}, headers);
+    if(res.code == 200) {
+      rateExchange.value.content = '1 ' + form.value.selectedCurrencyTo + ' ≈ ' + res.data + ' ' +form.value.selectedCurrency;
+      rateExchange.value.rate = res.data
+      console.log(rateExchange.value);
+    }else{
+      ElMessage.error(res.message || '查询失败');
+    }
+  } catch (error) {
+    ElMessage.error('请求失败，请重试');
+  }
+};
+
+// 手续费
+const fastRateFee = async (inputAmountTo: number, inputAmount: number, maxInputAmount: number, rate: number, isFlag: boolean) => {
+  try {
+    const [curRes, maxRes] = await Promise.all([
+      assetsApi.getFastRateFee({
+        currencyId: form.value.selectedCurrencyToId, currencyChain: form.value.selectedChainTo, amount: inputAmountTo,
+      }, headers),
+      assetsApi.getFastRateFee({
+        currencyId: form.value.selectedCurrencyToId, currencyChain: form.value.selectedChainTo, amount: maxInputAmount,
+      }, headers)
+    ]);
+
+    let fee = 0;
+    if(curRes.code == 200 && maxRes.code == 200) {
+      let curAmount = (inputAmountTo + curRes.data.fee).toFixed(8);
+      let maxAmount = (maxInputAmount - maxRes.data.fee).toFixed(8);
+      if(curAmount > maxAmount){
+        curAmount = maxAmount;
+        // fee = maxRes.data.fee;
+      }
+      fee = curRes.data.fee;
+      if(inputAmountTo < curAmount){
+        form.value.inputAmountTo = inputAmountTo;
+      }else{
+        // form.value.inputAmountTo = curAmount;
+      }
+      cost.value.content = fee + ' ' + form.value.selectedCurrencyTo
+      cost.value.amount = fee;
+      form.value.inputAmount = (form.value.inputAmountTo * rate).toFixed(8);
+      loading.value = false;
+    }
+  } catch (error) {
+    ElMessage.error('请求失败，请重试');
+    loading.value = false;
+  }
+};
+
+// 输入框同步输出金额
+const syncInputAmount = () => {
+  const rate = parseFloat(rateExchange.value.rate);
+  const inputAmount = parseFloat(form.value.inputAmount);
+  if (!isNaN(inputAmount) && !isNaN(rate)) {
+    // 保留 8 位小数
+    form.value.inputAmountTo = (inputAmount / rate).toFixed(8);
+  } else {
+    form.value.inputAmountTo = '';
+  }
+};
+// 输出框同步输入金额
+const syncInputAmountTo = (isFlag: boolean) => {
+  const rate = rateExchange.value.rate;
+  let inputAmountTo = parseFloat(form.value.inputAmountTo);
+  if(isFlag){
+    inputAmountTo = parseFloat(form.value.inputAmount);
+  }
+  const maxInputAmount = form.value.bigNumCost;
+  if (!isNaN(inputAmountTo) && !isNaN(rate)) {
+    fastRateFee(inputAmountTo, 0, maxInputAmount, rate, isFlag);
+  }else{
+    form.value.inputAmount = '';
+    loading.value = false;
+  }
+};
+// 初始化数据
+onMounted(() => {
+  fetchData();
+});
+
+// 交换货币和链
+const swapCurrencies = async () => {
+  loading.value = true;
+  const selectedCurrencyId = form.value.selectedCurrencyId;
+  const selectedCurrency = form.value.selectedCurrency;
+  const selectedChain = form.value.selectedChain;
+  const selectedCurrencyChain = form.value.selectedCurrencyChain;
+  // 重新计算金额
+  form.value.selectedCurrencyId = form.value.selectedCurrencyToId;
+  form.value.selectedCurrency = form.value.selectedCurrencyTo;
+  form.value.selectedChain = form.value.selectedChainTo;
+  form.value.selectedCurrencyChain = form.value.selectedCurrencyChainTo;
+  form.value.selectedCurrencyToId = selectedCurrencyId;
+  form.value.selectedCurrencyTo = selectedCurrency;
+  form.value.selectedChainTo = selectedChain;
+  form.value.selectedCurrencyChainTo = selectedCurrencyChain;
+
+  // 重新计算汇率
+  const  selectedCurrencyData = currencyMergedData.value.find(currency => currency.currencyId === form.value.selectedCurrencyToId)
+  form.value.bigNumCost = selectedCurrencyData.balance
+  fetchRateExchange();
+  // 延迟200毫秒
+  setTimeout(() => {
+    syncInputAmountTo(true);
+  }, 500);
+};
+
 
 </script>
 
-<style lang="less" scoped>
-.page{
-  position: relative;
-  height: calc(100vh - 18px);
-}
-.content{
-  padding-bottom: 20px;
-  position: relative;
-  .button{
-    position: absolute;
-    top: 118px;
-    left: 50%;
-    transform: translate(-50%);
-    width: 34px;
-    height: 34px;
-    border-radius: 34px;
-    background: #ffffff;
-    border: 3px #C8DCE8 solid;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .button img {
-      max-width: 16.5px;
-    }
-  }
-}
-
-.exchange-container {
-  border-radius: 16px;
-  background: #ffffff;
-  border: 3px #C8DCE8 solid;
-  padding-left: 16px;
-  .title {
-    font-size: 14px;
-    margin-top: 18px;
-  }
-  .flash-btn{
-    margin-top: 15px;
-    width: 42%;
-    span{
-      display: block;
-      width: 46px;
-      height: 24px;
-      font-size: 12px;
-      border-radius: 12px;
-      line-height: 24px;
-      text-align: center;
-      color: #333333;
-      background: #EAF3FA;
-    }
-  }
-  .currency{
-    font-size: 20px;
-    line-height: 22px;
-    overflow: hidden;
-    font-weight: bold;
-    margin-top: 5px;
-  }
-  .select-wrap{
-    font-size: 14px;
-    width: 110px;
-    height: 34px;
-    line-height: 34px;
-    border-radius: 6px;
-    color: #0D0D0D;
-    background: #F4F4F4;
-    position: absolute;
-    top: 50px;
-    right: 10px;
-    display: flex;
-    align-items: center;
-    overflow: hidden;
-    .img {
-      width: 18px;
-      height: 18px;
-      border-radius: 50%;
-      border: #D8D8D8 2px solid;
-      margin: 5px;
-      flex-shrink: 0;
-    }
-    .text-wrap {
-      flex: 1;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-    .icon {
-      width: 22px;
-      flex-shrink: 0;
-    }
-  }
-}
-
-.tips{
-  line-height: 30px;
-  margin-top: 5px;
-  width: 100%;
-  color: #666666;
-  font-size: 16px;
+<style scoped>
+.exchange-wrapper {
   display: flex;
-  justify-content: space-between;
+  align-items: center; /* 垂直居中对齐 */
 }
-.custom-button{
-  position: absolute;
-  bottom: 25px;
-  left: 50%;
-  transform: translate(-50%);
+.exchange-container-wrap{
+  width: 800px !important;;
+  height:350px !important;
+  margin-bottom: 20px !important;
+}
+.exchange-container {
+  height: 180px;
+  width: 340px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  background-color: white;
+  padding: 30px;
+  margin: 0 10px; /* 添加左右间距 */
+}
+.button-container {
+  display: flex;
+  align-items: center; /* 垂直居中对齐 */
+}
+.select-container {
+  display: flex;
+  flex-direction: column; /* 垂直排列 */
+}
+.input-container {
+  display: flex;
+  align-items: center; /* 垂直居中对齐 */
+}
+.balance-display {
+  text-align: right;
+  padding-top: 5px;
 }
 </style>
