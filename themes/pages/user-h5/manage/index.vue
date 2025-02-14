08e6919025faa8d5a45c9580b87da9503dc7522f
yoney-zhang <yoney.zhang@autech.one>
2025-02-14 15:27:24 +0800

    Google 测试账户

--- themes/pages/user-h5/manage/index.vue
+++ themes/pages/user-h5/manage/index.vue
@@ -3,20 +3,20 @@
       <div class="user-title-wrap">
         <div class="row">
           <span class="h1">用户中心</span>
-          <el-icon><el-image :src="shape" /></el-icon>
+          <el-icon size="20"><el-image :src="info" /></el-icon>
         </div>
       <div class="table-list">
         <div class="title">管理设置</div>
-        <div class="text"><el-image :src="shape" />账号信息</div>
-        <div class="text">用户信息</div>
-        <div class="text">申请/审批</div>
-        <div class="text">地址管理</div>
-        <div class="text">订单管理</div>
-        <div class="title">通讯录</div>
-        <div class="text">关于auPay</div>
-        <div class="text">关于我们</div>
-        <div class="text">隐私条款</div>
-        <div class="text">服务条款</div>
+        <div class="text" @click="jumpPage('/user', {})"><span><el-image :src="wallet" />账号信息</span><el-icon><ArrowRightBold /></el-icon></div>
+        <div class="text"><span><el-image :src="peo" />用户信息</span><el-icon><ArrowRightBold /></el-icon></div>
+        <div class="text"><span><el-image :src="editSquare" />申请/审批</span><el-icon><ArrowRightBold /></el-icon></div>
+        <div class="text"><span><el-image :src="add" />地址管理</span><el-icon><ArrowRightBold /></el-icon></div>
+        <div class="text"><span><el-image :src="order" />订单管理</span><el-icon><ArrowRightBold /></el-icon></div>
+        <div class="text"><span><el-image :src="addressbook" />通讯录</span><el-icon><ArrowRightBold /></el-icon></div>
+        <div class="title">关于auPay</div>
+        <div class="text"><span><el-image :src="settings" />关于我们</span><el-icon><ArrowRightBold /></el-icon></div>
+        <div class="text"><span><el-image :src="settings" />隐私条款</span><el-icon><ArrowRightBold /></el-icon></div>
+        <div class="text"><span><el-image :src="settings" />服务条款</span><el-icon><ArrowRightBold /></el-icon></div>
 
       </div>
       <div class="footer">auPay @2025.001版</div>
@@ -29,53 +29,30 @@
 import { ref, onMounted } from 'vue';
 import { getHeader } from "@/utils/storageUtils";
 import { useRouter, useRoute } from 'vue-router';
-import shape from '@@/public/images/Shape.svg'
 import wallet from "@@/public/images/user/Wallet.svg";
 import peo from "@@/public/images/user/peo.svg";
 import editSquare from "@@/public/images/user/EditSquare.svg";
 import add from "@@/public/images/user/add.svg";
 import order from "@@/public/images/user/Order.svg";
-import addressbook from "~~/public/images/user/Addressbook.svg";
-
+import addressbook from "@@/public/images/user/Addressbook.svg";
+import info from "@@/public/images/user/info.svg";
+import settings from "@@/public/images/user/Settings.svg";
+import {ArrowRightBold} from "@element-plus/icons-vue";
 
 const headers = getHeader();
 const router = useRouter();
 const { assetsApi, systemApi } = useServer();
-const currencyMergedData = ref([]);
-const totalAssets = ref(0);
 const isShowCurrency = ref(false);
 
-// 初始化
-const fetchData = async () => {
-  try {
-    const [rateRes, assetsRes] = await Promise.all([
-      assetsApi.getRateU2Currency({ currency: userInfo.value.currencyCode }, headers),
-      assetsApi.accountAssets({}, headers)
-    ]);
-    // 处理汇率响应
-    let exchangeRate = rateRes.code == 200?rateRes.data:1;
-    // 处理资产响应
-    if (assetsRes.code == 200) {
-
-    } else {
-      showErrorMessage(assetsRes.code, assetsRes.message)
-    }
-  } catch (error) {
-    showCatchErrorMessage()
-  }
-};
-// 显示隐藏
-const switchConfigShow = async () => {
-  isShowCurrency.value = !isShowCurrency.value;
+// 跳转列表首页
+const jumpPage= (url: string, query: any) => {
+  router.push({ path: url, query: query });
 }
+
+
 // 初始化数据
 onMounted(() => {
-  // const userStore = UseUserStore();
-  // userInfo.value.headPortrait = userStore.userInfo.headPortrait;
-  // userInfo.value.currencyCode = userStore.userInfo.currencyUnit;
-  // isShowCurrency.value = userStore.userInfo.showHide == 1;
-  // userInfo.value.name = userStore.userInfo.nickname;
-  // fetchData();
+
 })
 
 </script>
@@ -92,6 +69,7 @@ onMounted(() => {
     .h1{
       font-size: 26px;
     }
+
   }
   .footer{
     text-align: center;
@@ -99,12 +77,39 @@ onMounted(() => {
     padding-top: 50px;
   }
   .table-list{
+    div{
+      display: flex;
+      justify-content: space-between;
+      height: 38px;
+      line-height: 38px;
+      margin: 15px 0;
+    }
     .title{
       color: #333333;
     }
     .text{
+      span{
+        padding-left: 5px;
+        .el-image{
+          float: left;
+          margin-top: 0px;
+          margin-right: 8px;
+          width: 20px;
+        }
+      }
       color: #666666;
     }
   }
+  .footer{
+    position: fixed;
+    text-align: center;
+    font-size: 12px;
+    color: #999999;
+    left: 0;
+    right: 0;
+    bottom: 100px;
+    text-align: center;
+  }
 }
+
 </style>
